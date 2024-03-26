ab84914d0ec57f7c6ea4b941ac7d152534434f94
zdravko12 <zdravkoena@yahoo.com>
2024-03-26 23:13:30 +0100

    Zdravko PhotoGraphy Online Shop

---
+++ js/AnimationText.js
@@ -0,0 +1,57 @@
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var header = document.querySelector('.text1Home');
+    if (scrollPosition > 100) { // Adjust as needed
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var highlightedElements = document.querySelectorAll('.TextNewAnimation');
+    
+    highlightedElements.forEach(function(element) {
+      if (scrollPosition > 105) {
+        element.classList.add('highlight');
+      } else {
+        element.classList.remove('highlight');
+      }
+    });
+  });
+
+
+  let logo = document.getElementById('LogoZP');
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+        if (scrollTop > lastScrollTop) {
+            // Scrolling down
+            logo.classList.add('scrolling-up');
+        } else {
+            // Scrolling up
+            logo.classList.remove('scrolling-up');
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    });
+
+
+
+    window.addEventListener('scroll', function() {
+        var button = document.getElementById('ButtonColor');
+        var scrollPosition = window.scrollY;
+      
+        // Add a condition to change the button color when scrolling down
+        if (scrollPosition > 0) { 
+          button.style.backgroundColor = '#c7dc0d'; // Dark yellow
+          button.style.color = '#000'; // White 
+        } else {
+          // Reset to initial color if scrolling back to the top
+          button.style.backgroundColor = '#dcba0d'; // Light gray
+          button.style.color = '#000'; // Black
+        }
+      });
---
+++ js/HomejavaScript.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    var images = [
+        "/Images/FrameZp10.jpg",
+        "/Images/FrameZp9.jpg",
+        // Add more image URLs as needed
+    ];
+
+    var currentIndex = 0;
+
+    function slideShow() {
+        $('#slideshow img').attr('src', images[currentIndex]);
+        $('#slideshow img').removeClass('active');
+        currentIndex = (currentIndex + 1) % images.length;
+        $('#slideshow img').addClass('active');
+        setTimeout(slideShow, 3000); // Change slide every 3 seconds
+    }
+
+    slideShow();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var endDate = new Date();
+    endDate.setDate(endDate.getDate() + 10); // Set the end date 10 days from now
+  
+    var countdown = document.getElementById("countdown");
+  
+    // Update the countdown every second
+    var timer = setInterval(function() {
+      var now = new Date().getTime();
+      var distance = endDate - now;
+  
+      if (distance < 0) {
+        clearInterval(timer);
+        countdown.innerHTML = "EXPIRED";
+        return;
+      }
+      
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+      countdown.innerHTML =  days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    }, 1000);
+  });
